c530e1e0ce0e40fab7545f4eb23754c04f1102fb
aunmegustas <cralperezhe@ittepic.edu.mx>
2022-11-28 12:43:42 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u3_practica1_bdmixta/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.ittepic.ladm_u3_practica1_bdmixta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
