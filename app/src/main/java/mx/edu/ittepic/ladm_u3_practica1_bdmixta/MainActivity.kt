0408ab987416fd17c0ecfb98972962013494ace3
Chirstian Alexis <cralperezhe@ittepic.edu.mx>
2022-11-29 17:23:46 -0700

    Actualizacion: CRUD en Firebase, detección de conexión y cambio de CRUD al detectar conexión a internet, FALTA pasar datos cuando se conecte a internet.

--- app/src/main/java/mx/edu/ittepic/ladm_u3_practica1_bdmixta/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u3_practica1_bdmixta/MainActivity.kt
@@ -4,16 +4,24 @@ import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
 import android.net.NetworkInfo
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
+import androidx.core.view.size
+import androidx.lifecycle.Observer
+import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
+import java.net.UnknownHostException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.Instant
@@ -22,98 +30,159 @@ import java.time.Instant
 class MainActivity : AppCompatActivity() {
     var db = FirebaseFirestore.getInstance()
     val baseDatos = BaseDatos(this,"Tecnologico",null,1)
-    var IDs = ArrayList<Int>()
+    var IDs = ArrayList<Any?>()
+    var nube = ""
 
+    fun conectada() {
+        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+        if (isConnected) {
+            nube = "Firebase"
+            Toast.makeText(this, "CONECTADO", Toast.LENGTH_LONG).show()
+        } else {
+            nube = "SQLite"
+            Toast.makeText(this, "DESCONECTADO", Toast.LENGTH_LONG).show()
+        }
+    }
 
     @RequiresApi(Build.VERSION_CODES.O) //Se requiere para obtener la fecha y hora del dispositivo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mostrarTodos()
         conectada()
+        mostrarTodos(nube)
 
+        var internet = NetworkConnection(applicationContext)
+        internet.observe(this) { isConnected ->
+            if (isConnected) {
+                nube = "Firebase"
+                Toast.makeText(this, "CONECTADO", Toast.LENGTH_LONG).show()
+                if(listaAlumnos.size != 0) AlertDialog.Builder(this).setMessage(listaAlumnos.getItemAtPosition(0).toString()).show()
+            } else {
+                nube = "SQLite"
+                Toast.makeText(this, "DESCONECTADO", Toast.LENGTH_LONG).show()
+            }
+            mostrarTodos(nube)
 
+        }
 
-       insertar.setOnClickListener {
-
+        insertar.setOnClickListener {
            val dateTime = LocalDateTime.now()
                .format(DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mm:ss: a"))
-
-
            var alumno = baseDatos.writableDatabase
-           var datos = ContentValues()
+           var datosSQLite = ContentValues()
+           var datosFirebase: HashMap<String, Any>?
 
            if(nombre.text.toString()=="" || escuela.text.toString()=="" || telefono.text.toString()=="" || carrera1.text.toString()==""
                || carrera2.text.toString()=="" || correo.text.toString()==""){
                Toast.makeText(this,"COMPLETE TODOS LOS CAMPOS",Toast.LENGTH_LONG).show()
            }else{
-           datos.put("NOMBRE",nombre.text.toString())
-           datos.put("ESCUELAPROCEDENCIA",escuela.text.toString())
-           datos.put("TELEFONO",telefono.text.toString().toInt())
-           datos.put("CARRERA1",carrera1.text.toString())
-           datos.put("CARRERA2",carrera2.text.toString())
-           datos.put("CORREO",correo.text.toString())
-           datos.put("FECHA",dateTime.toString())
-
-           var resultado = alumno.insert("CANDIDATO","ID",datos)
-           if(resultado ==-1L){
-               AlertDialog.Builder(this).setTitle("ERROR")
-                   .setMessage("NO SE PUDO GUARDAR").show()
-           }else{
-               Toast.makeText(this,"SE INSERTO CORRECTAMENTE",Toast.LENGTH_LONG)
-                   .show()
-               nombre.setText("")
-               escuela.setText("")
-               telefono.setText("")
-               carrera1.setText("")
-               carrera2.setText("")
-               correo.setText("")
-               mostrarTodos()
-
+                var resultado = 0L
+                if(nube == "Firebase"){
+                    datosFirebase = hashMapOf("NOMBRE" to nombre.text.toString(), "ESCUELAPROCEDENCIA" to escuela.text.toString(),
+                        "TELEFONO" to telefono.text.toString(), "CARRERA1" to carrera1.text.toString(),
+                        "CARRERA2" to carrera2.text.toString(), "CORREO" to correo.text.toString(),
+                        "FECHA" to dateTime)
+                    db.collection("Candidatos")
+                        .add(datosFirebase)
+                        .addOnSuccessListener {
+                            resultado = 1
+                        }
+                }else if(nube == "SQLite"){
+                    datosSQLite.put("NOMBRE",nombre.text.toString())
+                    datosSQLite.put("ESCUELAPROCEDENCIA",escuela.text.toString())
+                    datosSQLite.put("TELEFONO",telefono.text.toString().toInt())
+                    datosSQLite.put("CARRERA1",carrera1.text.toString())
+                    datosSQLite.put("CARRERA2",carrera2.text.toString())
+                    datosSQLite.put("CORREO",correo.text.toString())
+                    datosSQLite.put("FECHA",dateTime.toString())
+                    resultado = alumno.insert("CANDIDATO","ID",datosSQLite)
                 }
-           }
+                if(resultado ==-1L){
+                    AlertDialog.Builder(this).setTitle("ERROR")
+                        .setMessage("NO SE PUDO GUARDAR")
+                        .setPositiveButton("Ok"){_,_->}
+                        .show()
+                }else{
+                    Toast.makeText(this,"SE INSERTO CORRECTAMENTE",Toast.LENGTH_LONG)
+                        .show()
+                    nombre.setText("")
+                    escuela.setText("")
+                    telefono.setText("")
+                    carrera1.setText("")
+                    carrera2.setText("")
+                    correo.setText("")
+                    mostrarTodos(nube)
+                }
+           }//ELSE VALIDACION DE CAMPOS
 
-       }//INSERTAR
+       }//INSERTAR SET ON CLICK LISTENER
 
 
     }
 
 
-    fun mostrarTodos() {
-        var alumno = baseDatos.readableDatabase
-        val lista = ArrayList<String>()
-
-
+    fun mostrarTodos(nube: String) {
+        var lista = ArrayList<String>()
         IDs.clear()
-        var resultado = alumno.query("CANDIDATO", arrayOf("*"), null, null, null, null, null)
-        if (resultado.moveToFirst()) {
-            do {
-                val data = resultado.getString(1) + "\n" + resultado.getString(2) + " | " +
-                        resultado.getInt(3)+  " | " + resultado.getString(4) + " | " + resultado.getString(5)+
-                " | " + resultado.getString(6) +  " | " + resultado.getString(7)
-                lista.add(data)
-                IDs.add(resultado.getInt(0))
-            } while (resultado.moveToNext())
-        } else {
-            lista.add("LA TABLA ESTA VACIA")
-        }
-        listaAlumnos.adapter = ArrayAdapter<String>(this,
-            android.R.layout.simple_list_item_1, lista)
+        if(nube == "SQLite") {
+            var alumno = baseDatos.readableDatabase
+            var resultado = alumno.query("CANDIDATO", arrayOf("*"), null, null, null, null, null)
+            if (resultado.moveToFirst()) {
+                do {
+                    val data = resultado.getString(1) + "\n" + resultado.getString(2) + " | " + resultado.getInt(3) + " | " +
+                            resultado.getString(4) + " | " + resultado.getString(5) + " | " + resultado.getString(6) + " | " + resultado.getString(7)
+                    lista.add(data)
+                    IDs.add(resultado.getInt(0))
+                } while (resultado.moveToNext())
+            } else {
+                lista.add("LA TABLA DE SQLITE ESTA VACIA")
+            }
 
-        listaAlumnos.setOnItemClickListener {adapterView, view, i, l ->
-            val idSeleccionado = IDs.get(i)
-            var nombre = lista.get(i)
+        }else if(nube == "Firebase"){
+            db.collection("Candidatos")
+                .addSnapshotListener{ value, error ->
+                    if(error != null){
+                        AlertDialog.Builder(this)
+                            .setMessage("No se pudo realizar la consulta")
+                            .setPositiveButton("Ok"){_,_ ->}
+                            .show()
+                    }//IF ERROR
+                    for (documento in value!!){
+                        val cadena = documento.getString("NOMBRE") + "\n" + documento.getString("ESCUELAPROCEDENCIA")  + " | " +
+                                documento.getString("TELEFONO") + " | " +  documento.getString("CARRERA1") + " | " +
+                                documento.getString("CARRERA2") + " | " + documento.getString("CORREO") + " | " + documento.getString("FECHA")
+                        lista.add(cadena)
+                        Log.d("TEXTO","DOCUMENTO: $cadena")
+                        IDs.add(documento.id)
+
+                        listaAlumnos.adapter = ArrayAdapter(
+                            this,
+                            android.R.layout.simple_list_item_1, lista
+                        )
+                    }//FOR
+                }//ADD SNAPSHOTLISTENER
+        }// IF NUBE
+        listaAlumnos.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, lista
+        )
+
+        listaAlumnos.setOnItemClickListener { _, _, i, _ ->
+            val idSeleccionado = IDs[i]
+            var nombre = lista[i]
             nombre = nombre.substring(0, nombre.indexOf("\n")).uppercase()
 
             AlertDialog.Builder(this)
                 .setTitle("ATENCION")
                 .setMessage("QUE DESEAS HACER CON: ${nombre}?")
-                .setPositiveButton("NADA"){d,i->}
-                .setNegativeButton("ELIMINAR"){d,i->
-                    eliminar(idSeleccionado)
+                .setPositiveButton("NADA") { _, _ -> }
+                .setNegativeButton("ELIMINAR") { _, _ ->
+                    eliminar(idSeleccionado.toString())
                 }
-                .setNeutralButton("ACTUALIZAR"){d,i->
-                    actualizar(idSeleccionado)
+                .setNeutralButton("ACTUALIZAR") { _, _ ->
+                    actualizar(idSeleccionado.toString())
                 }
                 .show()
 
@@ -121,55 +190,50 @@ class MainActivity : AppCompatActivity() {
 
     }//MOSTRAR TODOS
 
-    private fun actualizar(idSeleccionado: Int){
+    private fun actualizar(idSeleccionado: String){
         val otraVentana = Intent(this,MainActivity2:: class.java)
-        otraVentana.putExtra("idSeleccionado", idSeleccionado.toString())
+        otraVentana.putExtra("idSeleccionado", idSeleccionado)
+        otraVentana.putExtra("nube", nube)
         startActivity(otraVentana)
-
     }
 
-    private fun eliminar(idSeleccionado: Int) {
-        val resultado = baseDatos.writableDatabase.delete("CANDIDATO",
-            "ID=?", arrayOf(idSeleccionado.toString()))
+    private fun eliminar(idSeleccionado: String) {
+        if(nube == "SQLite") {
+            val resultado = baseDatos.writableDatabase.delete(
+                "CANDIDATO",
+                "ID=?", arrayOf(idSeleccionado)
+            )
 
-        if(resultado == 0){
-            AlertDialog.Builder(this)
-                .setMessage("ERROR NO SE BORRO")
-                .show()
-        }else{
-            Toast.makeText(this,"SE BORRO CON EXITO", Toast.LENGTH_LONG).show()
-            mostrarTodos()
-        }
+            if (resultado == 0) {
+                AlertDialog.Builder(this)
+                    .setMessage("ERROR NO SE BORRO")
+                    .show()
+            } else {
+                Toast.makeText(this, "Se elimino correctamente!", Toast.LENGTH_LONG).show()
+                mostrarTodos(nube)
+            }
+        }else if(nube == "Firebase"){ //ELSE IF NUBE
+            db.collection("Candidatos")
+                .document(idSeleccionado)
+                .delete()
+                .addOnSuccessListener {
+                    Toast.makeText(this,"Se elimino correctamente!", Toast.LENGTH_LONG).show()
+                }
+                .addOnFailureListener{
+                    AlertDialog.Builder(this)
+                        .setMessage(it.message!!)
+                        .setPositiveButton("Ok"){_,_ ->}
+                        .show()
+                }
+        }//IF NUBE
+        mostrarTodos(nube)
 
     }//ELIMINAR
 
-    fun conectada() {
-
-        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
-                if (!isConnected) {
-                    Toast.makeText(
-                        this,
-                        "ERROR! No se pudo recuperar data desde NUBE",
-                        Toast.LENGTH_LONG
-                    ).show()
-
-                }else{
-                    Toast.makeText(this,"CONECTADA",Toast.LENGTH_LONG).show()
-                }
-
-            }//CONECTADA
-
-
     override fun onRestart(){
         super.onRestart()
-        mostrarTodos()
-
+        mostrarTodos(nube)
     }//on restart
-
-
-
-    }
+}
 
 
--- app/src/main/java/mx/edu/ittepic/ladm_u3_practica1_bdmixta/MainActivity2.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u3_practica1_bdmixta/MainActivity2.kt
@@ -5,54 +5,107 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
     val baseDatos = BaseDatos(this,"Tecnologico",null,1)
     var idSeleccionado = ""
-
+    var nube = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        nube = intent.extras!!.getString("nube")!!
         idSeleccionado = intent.extras!!.getString("idSeleccionado")!!
 
-        val cursor = baseDatos.readableDatabase.query("CANDIDATO", arrayOf("*"),"ID=?", arrayOf(idSeleccionado),null,null,null)
+        if (nube == "SQLite") {
+            val cursor = baseDatos.readableDatabase.query(
+                "CANDIDATO",
+                arrayOf("*"),
+                "ID=?",
+                arrayOf(idSeleccionado),
+                null,
+                null,
+                null
+            )
 
-        if(cursor.moveToFirst()){
-            act_nombre.setText(cursor.getString(1))
-            act_escuela.setText(cursor.getString(2))
-            act_telefono.setText(cursor.getInt(3).toString())
-            act_carrera1.setText(cursor.getString(4))
-            act_carrera2.setText(cursor.getString(5))
-            act_correo.setText(cursor.getString(6))
+            if (cursor.moveToFirst()) {
+                act_nombre.setText(cursor.getString(1))
+                act_escuela.setText(cursor.getString(2))
+                act_telefono.setText(cursor.getInt(3).toString())
+                act_carrera1.setText(cursor.getString(4))
+                act_carrera2.setText(cursor.getString(5))
+                act_correo.setText(cursor.getString(6))
 
-        }else{
-            act_nombre.setText("No hay datos")
-            act_escuela.setText("No hay datos")
-            act_telefono.setText("No hay datos")
-            act_carrera1.setText("No hay datos")
-            act_carrera2.setText("No hay datos")
-            act_correo.setText("No hay datos")
-            act_nombre.isEnabled=false
-            act_escuela.isEnabled=false
-            act_telefono.isEnabled=false
-            act_carrera1.isEnabled=false
-            act_carrera2.isEnabled=false
-            act_correo.isEnabled=false
-            actualizar.isEnabled=false
+            } else {
+                act_nombre.setText("No hay datos")
+                act_escuela.setText("No hay datos")
+                act_telefono.setText("No hay datos")
+                act_carrera1.setText("No hay datos")
+                act_carrera2.setText("No hay datos")
+                act_correo.setText("No hay datos")
+                act_nombre.isEnabled = false
+                act_escuela.isEnabled = false
+                act_telefono.isEnabled = false
+                act_carrera1.isEnabled = false
+                act_carrera2.isEnabled = false
+                act_correo.isEnabled = false
+                actualizar.isEnabled = false
+            }
+        }else if(nube == "Firebase"){
+            FirebaseFirestore.getInstance()
+                .collection("Candidatos")
+                .document(idSeleccionado)
+                .get()
+                .addOnSuccessListener {
+                    act_nombre.setText(it.getString("NOMBRE"))
+                    act_escuela.setText(it.getString("ESCUELAPROCEDENCIA"))
+                    act_telefono.setText(it.getString("TELEFONO"))
+                    act_carrera1.setText(it.getString("CARRERA1"))
+                    act_carrera2.setText(it.getString("CARRERA2"))
+                    act_correo.setText(it.getString("CORREO"))
+                }
         }
 
         actualizar.setOnClickListener {
-            val datos = ContentValues()
-            datos.put("NOMBRE",act_nombre.text.toString())
-            datos.put("ESCUELAPROCEDENCIA",act_escuela.text.toString())
-            datos.put("TELEFONO",act_telefono.text.toString().toInt())
-            datos.put("CARRERA1",act_carrera1.text.toString())
-            datos.put("CARRERA2",act_carrera2.text.toString())
-            datos.put("CORREO",act_correo.text.toString())
-            val res = baseDatos.writableDatabase.update("CANDIDATO",datos,"ID=?", arrayOf(idSeleccionado))
+            var res: Int? = null
+            if(nube == "SQLite") {
+                val datos = ContentValues()
+                datos.put("NOMBRE", act_nombre.text.toString())
+                datos.put("ESCUELAPROCEDENCIA", act_escuela.text.toString())
+                datos.put("TELEFONO", act_telefono.text.toString().toInt())
+                datos.put("CARRERA1", act_carrera1.text.toString())
+                datos.put("CARRERA2", act_carrera2.text.toString())
+                datos.put("CORREO", act_correo.text.toString())
+                res = baseDatos.writableDatabase.update(
+                    "CANDIDATO",
+                    datos,
+                    "ID=?",
+                    arrayOf(idSeleccionado)
+                )
+            }else if(nube == "Firebase"){
+                FirebaseFirestore.getInstance()
+                    .collection("Candidatos")
+                    .document(idSeleccionado)
+                    .update("NOMBRE", act_nombre.text.toString(),
+                        "ESCUELAPROCEDENCIA", act_escuela.text.toString(),
+                        "TELEFONO", act_telefono.text.toString(),
+                        "CARRERA1", act_carrera1.text.toString(),
+                        "CARRERA2", act_carrera2.text.toString(),
+                        "CORREO", act_correo.text.toString())
+                    .addOnSuccessListener {
+                        Toast.makeText(this,"Se actualizo con exito", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                    .addOnFailureListener{
+                        AlertDialog.Builder(this)
+                            .setMessage(it.message!!)
+                            .show()
+                    }
+            }
             if(res==0){
                 AlertDialog.Builder(this)
                     .setMessage("NO SE PUDO ACTUALIZAR")
@@ -70,7 +123,7 @@ class MainActivity2 : AppCompatActivity() {
                 act_correo.isEnabled=false
                 actualizar.isEnabled=false
 
-                actualizar.setText("SE ACTUALIZO")
+                actualizar.text = "SE ACTUALIZO"
 
             }
 
